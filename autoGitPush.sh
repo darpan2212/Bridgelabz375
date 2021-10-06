b5acd7a648dee963916013f67cc40b66222423f7
darpan2212 <darpan.madlani@gmail.com>
2021-10-06 21:11:11 +0530

    Steps are modified in txt and shell script

--- autoGitPush.sh
+++ autoGitPush.sh
@@ -4,5 +4,8 @@ git add .;
 echo "---------commit------------";
 git commit -m "$1";
 
+echo "---------Git pull-----------";
+git pull origin $2;
+
 echo "---------Git push------------";
 git push origin $2;
