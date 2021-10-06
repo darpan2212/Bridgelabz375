a4d246a7d78df033ef24211053ee728a463db843
darpan2212 <darpan.madlani@gmail.com>
2021-10-06 20:03:07 +0530

    Refactor - Employee parttime wage calculation, Add - Automatic git push

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -1,13 +1,17 @@
-isPresent=$((RANDOM%2));
+isPresent=$((RANDOM%3));
 workingHour=0;
 WAGE_PER_HOUR=20;
 if [ $isPresent -eq 0 ]
 then
 	echo "Employee is Absent";
 	workingHour=0;
-else
+elif [ $isPresent -eq 1 ]
+then
 	echo "Employee is present";
 	workingHour=8;
+else
+	echo "Employee is working as part time";
+	workingHour=4;
 fi
 wage=$(($WAGE_PER_HOUR * $workingHour));
 
