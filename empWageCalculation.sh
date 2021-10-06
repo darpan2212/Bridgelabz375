646534bd0a54bcbcd31845ec5fde49981a209488
Darpan Mandali <darpan@proclink.com>
2021-10-06 19:54:36 +0530

    Refactor - calculation of his wage

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -1,7 +1,14 @@
 isPresent=$((RANDOM%2));
+workingHour=0;
+WAGE_PER_HOUR=20;
 if [ $isPresent -eq 0 ]
 then
 	echo "Employee is Absent";
+	workingHour=0;
 else
 	echo "Employee is present";
+	workingHour=8;
 fi
+wage=$(($WAGE_PER_HOUR * $workingHour));
+
+echo "Employee has earned $wage $ today";
