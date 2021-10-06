1bd1a02d4a55d7e92d3a74a1b9cf5c4c318a0ea9
darpan2212 <darpan.madlani@gmail.com>
2021-10-06 20:47:35 +0530

    Refactor - Checking employee attendence with case statement

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -1,18 +1,21 @@
 isPresent=$((RANDOM%3));
 workingHour=0;
 WAGE_PER_HOUR=20;
-if [ $isPresent -eq 0 ]
-then
+case $isPresent in
+	0)
 	echo "Employee is Absent";
 	workingHour=0;
-elif [ $isPresent -eq 1 ]
-then
+	;;
+
+	1)
 	echo "Employee is present";
 	workingHour=8;
-else
 	echo "Employee is working as part time";
 	workingHour=4;
-fi
+	;;
+
+	2)
+esac;
 wage=$(($WAGE_PER_HOUR * $workingHour));
 
 echo "Employee has earned $wage $ today";
